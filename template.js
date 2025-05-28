e642118b67e197d4e21d765efa030d69764db07a
tazvy <144959053+tazvy@users.noreply.github.com>
2025-05-28 15:44:37 +0200

    Update template.js

--- template.js
+++ template.js
@@ -18,3 +18,10 @@ export default ({markup, css}) => {
           </style>
         </head>
        
+ <body style="margin:0">
+          <div id="root">${markup}</div>
+          <style id="jss-server-side">${css}</style>
+          <script type="text/javascript" src="/dist/bundle.js"></script>
+        </body>
+      </html>`
+}
